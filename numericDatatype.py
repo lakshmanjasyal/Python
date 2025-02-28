22312437d3872c87c1c4e278bcfebb2a190093a4
lakshman narayan <lakshmannarayan@lakshmans-MacBook-Air.local>
2025-02-28 22:13:44 +0530

    Added new files

---
+++ numericDatatype.py
@@ -0,0 +1,22 @@
+# num=3;
+# print(type(num))
+# num1=3.14
+# print(type(num1))
+# # print(3/2)
+# # print(3**2)
+# # print(3%2)
+# print(abs(-3))
+# print(round(3.75))
+# print(round(3.75,1))
+# # print(3==2)
+# # print(3!=2)
+# # print(3>2)
+# # print(3<2)
+# # print(3>=2)
+
+
+# Strings
+num2='100'
+num3='200'
+print(num2+" " +num3)
+print(int(num2)+int(num3))
