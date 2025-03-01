729aeaa1247f82c553224b6d500664f9e6cc14f4
lakshman narayan <lakshmannarayan@lakshmans-MacBook-Air.local>
2025-03-01 15:55:31 +0530

    a

---
+++ Basic Codes/ifelseif.py
@@ -0,0 +1,15 @@
+name=(input('Enter your name: '))
+if name=='Lakshman' or 'lakshu ':
+    print(name)
+else:
+    print('Not match')
+
+
+user='Admin'
+logged_in=False
+# if user=='Admin' and logged_in:
+if not logged_in:
+    print('Please Log In')
+    # print('Admin Page')
+else:
+    print('Welcome')
